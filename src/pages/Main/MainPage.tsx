759f0881644bd290070443d367927fd48e14e53f
nakyong82 <nakyong7770@gmail.com>
2024-08-28 03:07:27 +0900

    ✨Feat: 홈 조회 api 연결하기

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -5,17 +5,24 @@ import MainScoreList from "../../components/Main/ScoreList/MainScoreList";
 import MainUserInfo from "../../components/Main/UserInfo/MainUserInfo";
 import MainVisaInfo from "../../components/Main/VisaInfo/MainVisaInfo";
 import { Container, UserInfoContainer } from "./style";
+import { useGetHome } from "../../hooks/services/home/queries";
 
 const MainPage = () => {
+  const { isLoading, error, data } = useGetHome();
+
+  if (isLoading) return <div></div>;
+  if (error) return <div>에러남: {error.message}</div>;
+  if (data) console.log(data);
+
   return (
     <>
       <Container>
         <MainHeader />
         <UserInfoContainer>
-          <MainUserInfo />
+          <MainUserInfo homeInfo={data?.data?.data} />
         </UserInfoContainer>
-        <MainScoreList />
-        <MainVisaInfo />
+        <MainScoreList homeInfo={data?.data?.data} />
+        <MainVisaInfo homeInfo={data?.data?.data} />
       </Container>
       <MainFloatBtn />
       <Menu />
