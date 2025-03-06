324458cbae5541311c623b3a2492fb7608f79604
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-03-06 22:50:26 +0900

    ✨ feat: 문서 관리 페이지 비동기 로딩 처리 #224

--- src/pages/ApplicationDocuments/ApplicationDocumentsPage.tsx
+++ src/pages/ApplicationDocuments/ApplicationDocumentsPage.tsx
@@ -1,6 +1,7 @@
 import BottomButtonPanel from '@/components/Common/BottomButtonPanel';
 import Button from '@/components/Common/Button';
 import BaseHeader from '@/components/Common/Header/BaseHeader';
+import LoadingPostItem from '@/components/Common/LoadingPostItem';
 import PageTitle from '@/components/Common/PageTitle';
 import DocumentCardList from '@/components/Document/DocumentCardList';
 import { usePatchWritingDocumentFinish } from '@/hooks/api/useApplication';
@@ -24,41 +25,48 @@ const ApplicationDocumentsPage = () => {
         hasMenuButton={false}
         title="Application Documents"
       />
-      {!isPending && (
-        <section className="w-full bg-surface-secondary">
-          <PageTitle
-            title={`Your Resume,\nYour Next Opportunity 🚀`}
-            content={`Keep your resume updated and\ntrack your job applications in one place!`}
-          />
-          <DocumentCardList
-            documents={data?.data as DocumentsSummaryResponse}
-          />
-          <BottomButtonPanel>
-            {data?.data.part_time_employment_permits?.status ===
-              'CONFIRMATION' &&
-            data?.data.standard_labor_contract?.status === 'CONFIRMATION' &&
-            data?.data.integrated_application?.word_url &&
-            data?.data.is_completed === false ? (
-              <Button
-                type="large"
-                bgColor={'bg-[#FEF387]'}
-                fontColor="text-[#1E1926]"
-                title="Next"
-                isBorder={false}
-                onClick={() => submitDocuments(Number(currentPostId))}
-              />
-            ) : (
-              <Button
-                type="large"
-                bgColor="bg-[#F4F4F9]"
-                fontColor="text-[#bdbdbd]"
-                isBorder={false}
-                title="Next"
-              />
-            )}
-          </BottomButtonPanel>
-        </section>
-      )}
+      <section className="w-full bg-surface-secondary">
+        <PageTitle
+          title={`Your Resume,\nYour Next Opportunity 🚀`}
+          content={`Keep your resume updated and\ntrack your job applications in one place!`}
+        />
+        {isPending && (
+          <div className="w-full h-[65vh] flex items-center justify-center">
+            <LoadingPostItem />
+          </div>
+        )}
+        {!isPending && (
+          <>
+            <DocumentCardList
+              documents={data?.data as DocumentsSummaryResponse}
+            />
+            <BottomButtonPanel>
+              {data?.data.part_time_employment_permits?.status ===
+                'CONFIRMATION' &&
+              data?.data.standard_labor_contract?.status === 'CONFIRMATION' &&
+              data?.data.integrated_application?.word_url &&
+              data?.data.is_completed === false ? (
+                <Button
+                  type="large"
+                  bgColor={'bg-[#FEF387]'}
+                  fontColor="text-[#1E1926]"
+                  title="Next"
+                  isBorder={false}
+                  onClick={() => submitDocuments(Number(currentPostId))}
+                />
+              ) : (
+                <Button
+                  type="large"
+                  bgColor="bg-[#F4F4F9]"
+                  fontColor="text-[#bdbdbd]"
+                  isBorder={false}
+                  title="Next"
+                />
+              )}
+            </BottomButtonPanel>
+          </>
+        )}
+      </section>
     </div>
   );
 };
