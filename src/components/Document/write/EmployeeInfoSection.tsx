b73b95eb6e6f977f9e3aa7e68c0a87422d9b8c85
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-03-10 18:30:37 +0900

    ♻️ refactor: 유학생/고용주 입력 정보 컴포넌트 리팩토링 #228

--- src/components/Document/write/EmployeeInfoSection.tsx
+++ src/components/Document/write/EmployeeInfoSection.tsx
@@ -12,8 +12,9 @@ import {
   LaborContractEmployeeInfo,
 } from '@/types/api/document';
 import { GiggleAddress } from '@/types/api/users';
-import { getDetailAddress } from '@/utils/document';
+import { getDetailAddress, propertyToString } from '@/utils/document';
 import { renderMap } from '@/utils/map';
+import { Fragment } from 'react/jsx-runtime';
 
 const EmployeeInfoSection = ({
   employee,
@@ -25,6 +26,73 @@ const EmployeeInfoSection = ({
   const { account_type } = useUserStore();
   const isEmployer = account_type === UserType.OWNER;
   const language = isEmployer ? 'ko' : 'name';
+
+  // 공통 타이틀 렌더링 함수
+  const renderTitle = (key: string) => (
+    <div className="w-full flex flex-col gap-1">
+      <p className="button-2 text-text-alternative">
+        {type === DocumentType.PART_TIME_PERMIT
+          ? PartTimePermitFormInfo[key as PartTimePermitFormProperty][language]
+          : LaborContractEmployeeFormInfo[
+              key as LaborContractEmployeeInfoProperty
+            ][language]}
+      </p>
+    </div>
+  );
+
+  // 기본 값 렌더링 컴포넌트
+  const DefaultValueRenderer = ({ value }: { value: unknown }) => (
+    <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
+      {propertyToString(String(value)) === 'Null'
+        ? 'none'
+        : propertyToString(String(value))}
+    </div>
+  );
+
+  // 각 필드 타입별 렌더러 컴포넌트 매핑
+  const renderFieldContent = (key: string, value: unknown) => {
+    // 각 특수 케이스별 렌더링
+    switch (key) {
+      case LaborContractEmployeeInfoProperty.ADDRESS:
+        return renderMap(value as GiggleAddress);
+      case LaborContractEmployeeInfoProperty.SIGNATURE_BASE64:
+        return (
+          <div className="flex flex-col gap-4">
+            <div className="border border-border-alternative rounded-lg">
+              {value !== '' && (
+                <img
+                  src={`data:image/svg+xml;base64,${value}`}
+                  className="w-full h-full object-cover bg-white rounded-lg"
+                  alt="signature preview"
+                />
+              )}
+            </div>
+          </div>
+        );
+      default:
+        return <DefaultValueRenderer value={value} />;
+    }
+  };
+
+  // 특수 케이스에 대한 추가 컴포넌트 렌더링
+  const renderAdditionalContent = (key: string, value: GiggleAddress) => {
+    switch (key) {
+      case LaborContractEmployeeInfoProperty.ADDRESS:
+        return (
+          <div className="w-full flex flex-col gap-1">
+            <p className="button-2 text-text-alternative">
+              Detailed Address in korea
+            </p>
+            <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
+              {getDetailAddress(value as GiggleAddress)}
+            </div>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="w-full relative rounded-lg flex flex-col items center justify-center p-4 text-left body-3 bg-white">
       <div className="w-full self-stretch flex flex-col items-start jusitfy-center">
@@ -34,53 +102,14 @@ const EmployeeInfoSection = ({
           </p>
         </section>
         <div className="w-full self-stretch flex flex-col items-center justify-start text-left pt-4 gap-3">
-          {/* 추후 UI 재사용 위한 고용주 정보 property를 반복문으로 ui 나열 */}
           {Object.entries(employee).map(([key, value]) => (
-            <div className="w-full flex flex-col gap-1">
-              <p className="button-2 text-text-alternative">
-                {type === DocumentType.PART_TIME_PERMIT
-                  ? PartTimePermitFormInfo[key as PartTimePermitFormProperty][
-                      language
-                    ]
-                  : LaborContractEmployeeFormInfo[
-                      key as LaborContractEmployeeInfoProperty
-                    ][language]}
-              </p>
-              {!['address', 'signature_base64'].includes(key) && (
-                <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
-                  {value}
-                </div>
-              )}
-
-              {key === LaborContractEmployeeInfoProperty.ADDRESS &&
-                renderMap(value as GiggleAddress)}
-
-              {/* 별도 property가 없는 detailed address 예외 처리 */}
-              {key === LaborContractEmployeeInfoProperty.ADDRESS && (
-                <div className="w-full flex flex-col gap-1">
-                  <p className="button-2 text-text-alternative">
-                    Detailed Address in korea
-                  </p>
-                  <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
-                    {getDetailAddress(value as GiggleAddress)}
-                  </div>
-                </div>
-              )}
-              {/* 유학생 서명 */}
-              {key === LaborContractEmployeeInfoProperty.SIGNATURE_BASE64 && (
-                <div className="flex flex-col gap-4">
-                  <div className="border border-border-alternative rounded-lg">
-                    {value !== '' && (
-                      <img
-                        src={`data:image/svg+xml;base64,${value}`}
-                        className="w-full h-full object-cover bg-white rounded-lg"
-                        alt="signature preview"
-                      />
-                    )}
-                  </div>
-                </div>
-              )}
-            </div>
+            <Fragment key={key}>
+              <div className="w-full flex flex-col gap-1">
+                {renderTitle(key)}
+                {renderFieldContent(key, value)}
+              </div>
+              {renderAdditionalContent(key, value as GiggleAddress)}
+            </Fragment>
           ))}
         </div>
       </div>
--- src/components/Document/write/EmployerInfoSection.tsx
+++ src/components/Document/write/EmployerInfoSection.tsx
@@ -21,6 +21,7 @@ import Tag from '@/components/Common/Tag';
 import { renderMap } from '@/utils/map';
 import { useUserStore } from '@/store/user';
 import { UserType } from '@/constants/user';
+import { Fragment } from 'react/jsx-runtime';
 
 const EmployerInfoSection = ({
   employ,
@@ -32,6 +33,101 @@ const EmployerInfoSection = ({
   const { account_type } = useUserStore();
   const isEmployer = account_type === UserType.OWNER;
   const language = isEmployer ? 'ko' : 'name';
+
+  // 공통 타이틀 렌더링 함수
+  const renderTitle = (key: string) => (
+    <div className="w-full flex flex-col gap-1">
+      <p className="button-2 text-text-alternative">
+        {type === DocumentType.PART_TIME_PERMIT &&
+          PartTimeEmployPermitEmployerInfo[key as EmployerInfoProperty][
+            language
+          ]}
+        {type === DocumentType.LABOR_CONTRACT &&
+          LaborContractEmployerInfoNameMap[
+            key as LaborContractEmployerInfoProperty
+          ][language]}
+      </p>
+    </div>
+  );
+
+  // 기본 값 렌더링 컴포넌트
+  const DefaultValueRenderer = ({ value }: { value: unknown }) => (
+    <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
+      {propertyToString(String(value)) === 'Null'
+        ? 'none'
+        : propertyToString(String(value))}
+    </div>
+  );
+
+  // 각 필드 타입별 렌더러 컴포넌트 매핑
+  const renderFieldContent = (key: string, value: unknown) => {
+    // 각 특수 케이스별 렌더링
+    switch (key) {
+      case LaborContractEmployerInfoProperty.ADDRESS:
+        return (
+          <>
+            {renderMap(value as GiggleAddress)}
+            {/* Detailed Address 컴포넌트는 별도로 렌더링되므로 여기서는 반환하지 않음 */}
+          </>
+        );
+      case LaborContractEmployerInfoProperty.SIGNATURE_BASE64:
+        return (
+          <div className="w-full flex flex-col gap-4">
+            <div className="border border-border-alternative rounded-lg">
+              {value !== '' && (
+                <img
+                  src={`data:image/svg+xml;base64,${value}`}
+                  className="w-full h-full object-cover bg-white rounded-lg"
+                  alt="signature preview"
+                />
+              )}
+            </div>
+          </div>
+        );
+      case LaborContractEmployerInfoProperty.WEEKLY_LAST_DAYS:
+        return (
+          <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
+            {arrayToString(value as string[])}
+          </div>
+        );
+      case LaborContractEmployerInfoProperty.WORK_DAY_TIME_LIST:
+        return (
+          <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
+            <Tag
+              value={workDayTimeToString(value as WorkDayTime[])}
+              padding="py-[4px] px-[12px]"
+              isRounded
+              hasCheckIcon={false}
+              backgroundColor="bg-surface-primary"
+              color="text-text-strong"
+              fontStyle="button-2"
+            />
+          </div>
+        );
+      default:
+        return <DefaultValueRenderer value={value} />;
+    }
+  };
+
+  // 특수 케이스에 대한 추가 컴포넌트 렌더링
+  const renderAdditionalContent = (key: string, value: GiggleAddress) => {
+    switch (key) {
+      case LaborContractEmployerInfoProperty.ADDRESS:
+        return (
+          <div className="w-full flex flex-col gap-1">
+            <p className="button-2 text-text-alternative">
+              Detailed Address in Korea
+            </p>
+            <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
+              {getDetailAddress(value as GiggleAddress)}
+            </div>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="w-full relative rounded-lg flex flex-col items center justify-center p-4 text-left body-3 bg-white">
       <div className="w-full self-stretch flex flex-col items-start jusitfy-center">
@@ -41,90 +137,18 @@ const EmployerInfoSection = ({
           </p>
         </section>
         <div className="w-full flex flex-col gap-3 mt-4">
-          {/* 추후 UI 재사용 위한 고용주 정보 property를 반복문으로 ui 나열 */}
           {Object.entries(employ).map(([key, value]) => (
-            <>
+            <Fragment key={key}>
               <div className="w-full self-stretch flex flex-col text-left items-center justify-start">
-                {/* title */}
-                <div className="w-full flex flex-col gap-1">
-                  <p className="button-2 text-text-alternative">
-                    {type === DocumentType.PART_TIME_PERMIT &&
-                      PartTimeEmployPermitEmployerInfo[
-                        key as EmployerInfoProperty
-                      ][language]}
-                    {type === DocumentType.LABOR_CONTRACT &&
-                      LaborContractEmployerInfoNameMap[
-                        key as LaborContractEmployerInfoProperty
-                      ][language]}
-                  </p>
-                </div>
-                {/* value */}
-                {![
-                  'address',
-                  'signature_base64',
-                  'work_day_time_list',
-                  'weekly_last_days',
-                ].includes(key) && (
-                  <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
-                    {propertyToString(String(value)) === 'Null'
-                      ? 'none'
-                      : propertyToString(String(value))}
-                  </div>
-                )}
-                {key === LaborContractEmployerInfoProperty.ADDRESS &&
-                  renderMap(value as GiggleAddress)}
+                {renderTitle(key)}
+                {renderFieldContent(key, value)}
               </div>
-              {/* 별도 property가 없는 detailed address 예외 처리 */}
-              {key === LaborContractEmployerInfoProperty.ADDRESS && (
-                <div className="w-full flex flex-col gap-1">
-                  <p className="button-2 text-text-alternative">
-                    Detailed Address in Korea
-                  </p>
-                  <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
-                    {getDetailAddress(value as GiggleAddress)}
-                  </div>
-                </div>
-              )}
-              {/* 대표 서명 */}
-              {key === LaborContractEmployerInfoProperty.SIGNATURE_BASE64 && (
-                <div className="w-full flex flex-col gap-4">
-                  <div className="border border-border-alternative rounded-lg">
-                    {value !== '' && (
-                      <img
-                        src={`data:image/svg+xml;base64,${value}`}
-                        className="w-full h-full object-cover bg-white rounded-lg"
-                        alt="signature preview"
-                      />
-                    )}
-                  </div>
-                </div>
-              )}
-              {/* 주휴일 (배열 형태로 주어져 별도 가공 필요) */}
-              {key === LaborContractEmployerInfoProperty.WEEKLY_LAST_DAYS && (
-                <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
-                  {arrayToString(value as string[])}
-                </div>
-              )}
-              {/* 근무 요일과 시간(복잡한 객체 구조 및 태그로 변환해야 해 별도 가공) */}
-              {key === LaborContractEmployerInfoProperty.WORK_DAY_TIME_LIST && (
-                <div className="w-full self-stretch flex items-start justify-start body-2 text-primary-dark">
-                  <Tag
-                    value={workDayTimeToString(value as WorkDayTime[])}
-                    padding="py-[4px] px-[12px]"
-                    isRounded
-                    hasCheckIcon={false}
-                    backgroundColor="bg-surface-primary"
-                    color="text-text-strong"
-                    fontStyle="button-2"
-                  />
-                </div>
-              )}
-            </>
+              {renderAdditionalContent(key, value as GiggleAddress)}
+            </Fragment>
           ))}
         </div>
       </div>
     </div>
   );
 };
-
 export default EmployerInfoSection;
