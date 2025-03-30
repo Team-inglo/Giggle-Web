1baefa97f1ccc6ee25ca0910eded341e27c8173e
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-03-31 03:53:36 +0900

    ✨ feat: api 함수 양식 변경 및 타입 추가 #289

--- src/components/Common/ReactNativeMessageListener.tsx
+++ src/components/Common/ReactNativeMessageListener.tsx
@@ -1,19 +1,29 @@
 import { useEffect } from 'react';
 import { setupReactNativeMessageListener } from '@/utils/reactNativeMessage';
 import { usePatchDeviceToken } from '@/hooks/api/useAuth';
-
+interface FCMTokenPayload {
+  deviceToken: string;
+  deviceId: string;
+}
+interface ReactNativeMessage {
+  type: string;
+  payload?: string | FCMTokenPayload;
+}
 export const ReactNativeMessageListener = () => {
   const { mutate: patchDeviceToken } = usePatchDeviceToken();
 
+  // 메시지 리스너 함수
   useEffect(() => {
-    const cleanup = setupReactNativeMessageListener((data) => {
-      if (data.type === 'FCMTOKEN' && data.payload !== undefined) {
-        patchDeviceToken(data.payload);
-      }
-    });
+    const cleanup = setupReactNativeMessageListener(
+      (data: ReactNativeMessage) => {
+        if (data.type === 'FCMTOKEN' && data.payload !== undefined) {
+          const { deviceToken, deviceId } = data.payload as FCMTokenPayload;
+          patchDeviceToken({ deviceToken: deviceToken, deviceId: deviceId });
+        }
+      },
+    );
 
     return cleanup;
   }, [patchDeviceToken]);
-
   return null;
 };
