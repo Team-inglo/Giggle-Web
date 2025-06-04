98e55895fd7846e4f9b5b768dc9ddceaa9121774
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-06-04 20:34:52 +0900

    🎨 style: PageTitle 컴포넌트 변경에 따른 패딩 과도한 페이지 수정

--- src/pages/Information/InformationPage.tsx
+++ src/pages/Information/InformationPage.tsx
@@ -105,7 +105,7 @@ const InformationPage = () => {
               }`}
             />
           </div>
-          <div className="w-full flex justify-center px-4 pt-4">
+          <div className="w-full flex justify-center pt-4">
             {currentStep === 1 && (
               <InformationStep userInfo={userInfo} onNext={handleNext} />
             )}
