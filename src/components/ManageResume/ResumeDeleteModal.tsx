13685b73c3f7e0e8cb02599f85cf45d9ee2dc39b
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-06-18 01:47:46 +0900

    🐛 fix: 이력서 편집 모달 스와이프 종료 지원, 문구 변경 #404

--- src/components/ManageResume/ResumeDeleteModal.tsx
+++ src/components/ManageResume/ResumeDeleteModal.tsx
@@ -14,13 +14,13 @@ const ResumeDeleteModal = ({
 
   return (
     <BottomSheetLayout
-      isAvailableHidden={false}
+      isAvailableHidden={true}
       isShowBottomsheet={true}
       isFixedBackground={true}
     >
       <div className="w-full flex flex-col bg-white rounded-[1.125rem] overflow-hidden">
         <div>
-          <h1 className="pb-3 heading-18-semibold text-text-strong">
+          <h1 className="py-3 heading-18-semibold text-text-strong">
             Are you sure you want to delete?
           </h1>
         </div>
@@ -29,14 +29,14 @@ const ResumeDeleteModal = ({
             className="py-3 w-full flex justify-start items-center body-16-regular text-text-strong"
             onClick={onEditButton}
           >
-            수정하기
+            Edit
           </button>
           {onDeleteButton && (
             <button
               className="py-3 w-full flex justify-start items-start body-16-regular text-text-error"
               onClick={onDeleteButton}
             >
-              삭제하기
+              Delete
             </button>
           )}
         </div>
