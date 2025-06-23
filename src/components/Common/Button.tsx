2fde71b6600527a23c36670334581da09997a039
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-06-24 04:25:06 +0900

    ✨ feat: 버튼 디자인 변경 & 터치/클릭 시 인터렉션 추가 #419

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,9 +1,12 @@
 import CallIcon from '@/assets/icons/CallIcon.svg?react';
 import ScrapIcon from '@/assets/icons/Scrap.svg?react';
 import { buttonTypeKeys, buttonTypeUnion } from '@/constants/components';
+import { useState } from 'react';
+import PressOverlay from './PressedOverlay';
 
 type buttonProps = {
   type: buttonTypeUnion; // 정의된 버튼을 5가지 타입으로 나누었습니다.
+  size?: 'md' | 'lg';
   bgColor?: string; // 버튼의 배경색 (optional)
   fontColor?: string; // 버튼 글자색 (optional)
   isCallIcon?: boolean; // 전화기 아이콘 여부 (optional)
@@ -13,30 +16,59 @@ type buttonProps = {
 
 const Button = ({
   type,
+  size,
   bgColor,
   fontColor,
   isCallIcon = false,
   title,
   onClick,
 }: buttonProps) => {
+  const [isPressed, setIsPressed] = useState(false);
+  const handlePressStart = () => {
+    if (type !== buttonTypeKeys.DISABLED && type !== buttonTypeKeys.INACTIVE)
+      setIsPressed(true);
+  };
+
+  const handlePressEnd = () => {
+    setIsPressed(false);
+  };
   const getButtonStyle = () => {
     switch (type) {
       case buttonTypeKeys.LARGE:
-        return 'w-full py-4 flex justify-center items-center rounded-lg button-16-semibold';
+        return 'w-full py-4 flex justify-center items-center rounded-xl button-16-semibold';
       case buttonTypeKeys.SMALL:
-        return 'w-[24vw] py-3 flex justify-center items-center rounded button-14-semibold';
+        return 'w-[24vw] py-3 flex justify-center items-center rounded-lg button-14-semibold';
       case buttonTypeKeys.APPLY:
-        return `w-full py-4 flex justify-center items-center rounded-lg bg-neutral-100 bg-cover bg-center button-16-semibold text-neutral-100`;
+        return `w-full py-4 flex justify-center items-center rounded-xl bg-neutral-100 bg-cover bg-center button-16-semibold text-neutral-100`;
       case buttonTypeKeys.SMALLAPPLY: // 스크랩 버튼과 함께 쓰이는 Apply 버튼
         return `w-[71vw] py-4 flex justify-center items-center rounded-lg bg-neutral-100 bg-cover bg-center button-16-semibold text-neutral-100`;
       case buttonTypeKeys.BACK: // CONTINUE 버튼과 같은 열에 사용
-        return 'w-[31vw] py-4 flex justify-center items-center rounded-lg button-16-semibold';
+        return 'w-[31vw] py-4 flex justify-center items-center rounded-xl button-16-semibold';
       case buttonTypeKeys.CONTINUE: // BACK 버튼과 같은 열에 사용
-        return 'w-[53vw] py-4 flex justify-center items-center rounded-lg button-16-semibold';
+        return 'w-[53vw] py-4 flex justify-center items-center rounded-xl button-16-semibold';
       case buttonTypeKeys.SCRAP:
-        return 'p-4 flex justify-center items-center rounded-lg bg-[rgba(244,244,249,0.5)';
+        return 'p-4 flex justify-center items-center rounded-lg bg-[rgba(244,244,249,0.5)]';
+      case buttonTypeKeys.PRIMARY:
+        return 'bg-brand-500 text-text-strong';
+      case buttonTypeKeys.NEUTRAL:
+        return 'bg-neutral-100 text-text-strong';
+      case buttonTypeKeys.TERTIARY:
+        return 'bg-neutral-0 text-text-normal';
+      case buttonTypeKeys.DISABLED:
+        return 'bg-neutral-300 text-text-disabled';
+      case buttonTypeKeys.INACTIVE:
+        return 'text-text-disabled';
       default: // 기본값으로 large type 적용
-        return 'w-full py-4 flex justify-center items-center rounded-lg button-16-semibold';
+        return 'w-full px-5 py-4 flex justify-center items-center rounded-xl button-16-semibold';
+    }
+  };
+
+  const getButtonStyleBySize = () => {
+    switch (size) {
+      case 'md':
+        return 'w-full px-4 py-3 flex justify-center items-center rounded-xl button-14-semibold';
+      case 'lg':
+        return 'w-full px-5 py-4 flex justify-center items-center rounded-xl button-16-semibold';
     }
   };
 
@@ -45,24 +77,47 @@ const Button = ({
       {/* 스크랩 버튼 아이콘 처리 */}
       {type === buttonTypeKeys.SCRAP ? (
         <button
-          className={`${getButtonStyle()} bg-[rgba(244,244,249,0.5)`}
+          className={`${getButtonStyle()} relative transition-transform duration-150 ease-in-out ${
+            isPressed ? 'scale-95' : ''
+          }`}
           onClick={onClick}
+          onTouchStart={handlePressStart}
+          onTouchEnd={handlePressEnd}
+          onTouchCancel={handlePressEnd}
+          onMouseDown={handlePressStart}
+          onMouseUp={handlePressEnd}
+          onMouseLeave={handlePressEnd}
         >
           <ScrapIcon />
+          <PressOverlay isPressed={isPressed} buttonType={type} />
         </button>
       ) : (
         // 스크랩 버튼 이외 텍스트 버튼
         <button
-          className={`${getButtonStyle()} ${bgColor} ${fontColor}`}
+          className={`${
+            size && getButtonStyleBySize()
+          } ${getButtonStyle()} ${bgColor} ${fontColor} relative transition-transform duration-150 ease-in-out ${
+            isPressed ? 'scale-95' : ''
+          }`}
           onClick={onClick}
+          onTouchStart={handlePressStart}
+          onTouchEnd={handlePressEnd}
+          onTouchCancel={handlePressEnd}
+          onMouseDown={handlePressStart}
+          onMouseUp={handlePressEnd}
+          onMouseLeave={handlePressEnd}
         >
           {isCallIcon ? (
             <div className="flex justify-center items-center gap-1.5">
               <CallIcon />
               <div>{title}</div>
+              <PressOverlay isPressed={isPressed} buttonType={type} />
             </div>
           ) : (
-            <div>{title}</div>
+            <>
+              <div>{title}</div>
+              <PressOverlay isPressed={isPressed} buttonType={type} />
+            </>
           )}
         </button>
       )}
