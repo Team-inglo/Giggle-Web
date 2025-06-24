c64b568a9b23def37da07d73a101d16bb431978b
ohoCoding <youngho7482@gmail.com>
2025-06-24 20:59:11 +0900

    refactor: :recycle: 이미지 작아지는 버그 수정

--- src/components/Home/HomeCareerPostCard.tsx
+++ src/components/Home/HomeCareerPostCard.tsx
@@ -21,20 +21,19 @@ const HomeCareerPostCard = ({ careerData }: HomeCareerPostCardProps) => {
       className="flex flex-col gap-2 w-[9.063rem] m-1 rounded-lg"
       onClick={goToCareerDetailPage}
     >
-      {/* 썸네일 영역 (CareerListItemType에는 img가 없는 상태라 빈 박스로 처리) */}
-      <div className="w-full h-[6.75rem] rounded-lg border border-border-alternative bg-surface-secondary">
-        {careerData.img_urls && careerData.img_urls.length > 0 ? (
-          <img
-            src={careerData.img_urls[0] ? careerData.img_urls[0] : ''}
-            alt="career-thumbnail"
-            className="object-cover w-full h-full rounded-lg"
-          />
-        ) : (
-          <div className="flex items-center justify-center w-full h-full text-text-alternative">
-            No Image
-          </div>
-        )}
-      </div>
+      {/* 이미지 박스 - 기존 Job 공고와 동일하게 통일 */}
+      {careerData.img_urls && careerData.img_urls.length > 0 ? (
+        <div
+          className="w-full h-[6.75rem] min-w-[9.063rem] rounded-lg border border-border-alternative bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${careerData.img_urls[0]})`,
+          }}
+        ></div>
+      ) : (
+        <div className="w-full h-[6.75rem] min-w-[9.063rem] rounded-lg border border-border-alternative bg-surface-secondary flex items-center justify-center text-text-alternative">
+          No Image
+        </div>
+      )}
 
       <div className="block">
         {/* 제목 */}
@@ -42,26 +41,20 @@ const HomeCareerPostCard = ({ careerData }: HomeCareerPostCardProps) => {
           {careerData.title}
         </h3>
 
-        {/* 주관 - 주최 */}
-        <div className="flex items-center justify-between">
-          <p className="caption-12-regular text-text-alternative">
-            {careerData.organizer_name ?? '-'}
-          </p>
-          <p className="caption-12-regular text-text-alternative">
-            {careerData.host_name ?? '-'}
-          </p>
-        </div>
+        {/* 주관-주최 */}
+        <p className="whitespace-normal caption-12-regular text-text-alternative">
+          {careerData.organizer_name ?? '-'}
+          <span className="inline-block h-3 mx-1 align-middle border bg-border-alternative"></span>
+          {careerData.host_name ?? '-'}
+        </p>
 
-        {/* 하단 태그 */}
-        <div className="flex flex-wrap gap-1 mt-1">
-          {/* 커리어 카테고리 */}
+        {/* 태그 영역 */}
+        <div className="flex flex-wrap items-center gap-1 mt-1">
           {careerData.career_category && (
             <span className="caption-12-regular bg-[#0066FF1F] text-text-success py-[0.188rem] px-[0.25rem] rounded">
               {CAREER_CATEGORY[careerData.career_category]}
             </span>
           )}
-
-          {/* 비자 유형 */}
           {careerData.visa && careerData.visa.length > 0 && (
             <span className="caption-12-regular bg-surface-secondary text-text-alternative py-[0.188rem] px-[0.25rem] rounded">
               {careerData.visa.join(', ').replace(/_/g, '-')}
