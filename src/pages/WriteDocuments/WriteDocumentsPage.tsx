016077dd931be958e865a19da89effd7c0ca484a
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-08-21 16:04:17 +0900

    ✨ feat: 문서 작성 페이지 별 title 적용 #458

--- src/pages/WriteDocuments/WriteDocumentsPage.tsx
+++ src/pages/WriteDocuments/WriteDocumentsPage.tsx
@@ -1,8 +1,8 @@
 import BaseHeader from '@/components/Common/Header/BaseHeader';
-import DocumentSubHeader from '@/components/Document/write/DocumentSubHeader';
 import DocumentFormDispenser from '@/components/WriteDocuments/DocumentFormDispenser';
 import useNavigateBack from '@/hooks/useNavigateBack';
 import { DocumentType } from '@/types/api/document';
+import { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 
@@ -12,16 +12,28 @@ const WriteDocumentsPage = () => {
   const { type, isEdit, userOwnerPostId } = location.state || {};
   const { id } = useParams();
 
+  const title = useMemo(() => {
+    switch (type) {
+      case DocumentType.PART_TIME_PERMIT:
+        return 'Part-Time Employment Permit';
+      case DocumentType.INTEGRATED_APPLICATION:
+        return 'Integrated Application';
+      case DocumentType.LABOR_CONTRACT:
+        return 'Employment Contract';
+      default:
+        return 'Fill in document';
+    }
+  }, [type]);
+
   return (
     <div>
       <BaseHeader
         hasBackButton={true}
         hasMenuButton={false}
-        title="Fill in document"
+        title={title}
         onClickBackButton={handleClickBackButton}
       />
       <div className="relative">
-        <DocumentSubHeader type={type as DocumentType} />
         <DocumentFormDispenser
           type={type as DocumentType}
           isEdit={isEdit}
