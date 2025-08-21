d014db230a604844f996dd5316d465b54387e326
Savien/Woo Jun Han <49388937+MrMirror21@users.noreply.github.com>
2025-08-21 16:07:50 +0900

    ✨ feat: 표준근로계약서 작성 step 분리 #458

--- src/components/WriteDocuments/LaborContractWriteForm.tsx
+++ src/components/WriteDocuments/LaborContractWriteForm.tsx
@@ -23,20 +23,159 @@ import ValidatedSubmitButton from '@/components/Document/write/ValidatedSubmitBu
 import { renderField } from '@/components/Document/write/renderField';
 import Button from '@/components/Common/Button';
 import { initialLaborContractEmployeeInfo } from '@/constants/documents';
-
-// 필수 검증 필드 목록
-const REQUIRED_FIELDS: Array<keyof LaborContractEmployeeInfo | 'phone'> = [
-  'first_name',
-  'last_name',
-  'phone',
-  'address',
-  'signature_base64',
-];
+import Icon from '@/components/Common/Icon';
+import DocumentIcon from '@/assets/icons/Image Container.svg?react';
+import InfoBanner from '@/components/Common/InfoBanner';
+import { InfoBannerSize, InfoBannerState } from '@/types/common/infoBanner';
+import { useMemo, useState } from 'react';
+import ProgressStepper from '@/components/Common/ProgressStepper';
+import PageTitle from '@/components/Common/PageTitle';
 
 type LaborContractFormProps = {
   document?: LaborContractDataResponse;
   isEdit: boolean;
   userOwnerPostId: number;
+};
+
+const Step1 = ({ setStep }: { setStep: (step: number) => void }) => {
+  const REQUIRED_FIELDS: Array<keyof LaborContractEmployeeInfo | 'phone'> = [
+    'first_name',
+    'last_name',
+    'phone',
+    'address',
+  ];
+  // 필드 타입에 따른 입력 컴포넌트 렌더링
+  const renderFormField = (field: LaborContractFormField) => {
+    return renderField<LaborContractEmployeeInfo>({
+      field,
+      name: field.name as keyof LaborContractEmployeeInfo,
+    });
+  };
+  return (
+    <div className="w-full pb-6 flex flex-col">
+      <div className="[&>*:last-child]:mb-24 flex flex-col gap-6">
+        {LaborContractFormFields.step1.map((field) => (
+          <InputLayout key={field.name} title={field.title}>
+            {renderFormField(field)}
+          </InputLayout>
+        ))}
+      </div>
+      <BottomButtonPanel>
+        {/* 입력된 정보의 유효성 검사 통과 시 활성화 */}
+        <ValidatedSubmitButton
+          fieldNames={REQUIRED_FIELDS}
+          validationFn={(data) => validateLaborContract(data, 'step1')}
+          onClick={() => setStep(2)}
+        >
+          <Button
+            type={Button.Type.PRIMARY}
+            size={Button.Size.LG}
+            isFullWidth
+            title={'Next'}
+          />
+        </ValidatedSubmitButton>
+      </BottomButtonPanel>
+    </div>
+  );
+};
+
+const Step2 = ({
+  setStep,
+  document,
+}: {
+  setStep: (step: number) => void;
+  document: LaborContractDataResponse;
+}) => {
+  // 필수 검증 필드 목록
+  return (
+    <>
+      <div className="[&>*:last-child]:mb-24 flex flex-col gap-6">
+        {/* 고용주 정보가 있다면 표시 */}
+        {document?.employer_information ? (
+          <EmployerInfoSection
+            employ={document.employer_information}
+            type={DocumentType.LABOR_CONTRACT}
+          />
+        ) : (
+          <div className="flex h-48 bg-surface-disabled items-center justify-center rounded-lg">
+            <Icon icon={DocumentIcon} />
+          </div>
+        )}
+      </div>
+      <BottomButtonPanel>
+        <div className="w-full flex gap-2">
+          <Button
+            type={Button.Type.NEUTRAL}
+            layout={Button.Layout.SMALL_BUTTON}
+            size={Button.Size.LG}
+            title="Previous"
+            onClick={() => setStep(1)}
+          />
+          <Button
+            type={Button.Type.PRIMARY}
+            size={Button.Size.LG}
+            isFullWidth
+            title={'Next'}
+            onClick={() => setStep(3)}
+          />
+        </div>
+      </BottomButtonPanel>
+    </>
+  );
+};
+
+const Step3 = ({
+  setStep,
+  onSubmit,
+}: {
+  setStep: (step: number) => void;
+  onSubmit: () => void;
+}) => {
+  const REQUIRED_FIELDS: Array<keyof LaborContractEmployeeInfo | 'phone'> = [
+    'signature_base64',
+  ];
+  // 필드 타입에 따른 입력 컴포넌트 렌더링
+  const renderFormField = (field: LaborContractFormField) => {
+    return renderField<LaborContractEmployeeInfo>({
+      field,
+      name: field.name as keyof LaborContractEmployeeInfo,
+    });
+  };
+  return (
+    <div className="w-full pb-6 flex flex-col">
+      <div className="[&>*:last-child]:mb-24 flex flex-col gap-6">
+        {LaborContractFormFields.step3.map((field) => (
+          <InputLayout key={field.name} title={field.title}>
+            {renderFormField(field)}
+          </InputLayout>
+        ))}
+      </div>
+      <BottomButtonPanel>
+        {/* 입력된 정보의 유효성 검사 통과 시 활성화 */}
+        <div className="w-full flex gap-2">
+          <Button
+            type={Button.Type.NEUTRAL}
+            layout={Button.Layout.SMALL_BUTTON}
+            size={Button.Size.LG}
+            title="Previous"
+            onClick={() => setStep(2)}
+          />
+          <ValidatedSubmitButton
+            fieldNames={REQUIRED_FIELDS}
+            validationFn={(data) => validateLaborContract(data, 'step3')}
+            onClick={() => onSubmit}
+          >
+            <Button
+              type={Button.Type.PRIMARY}
+              size={Button.Size.LG}
+              isFullWidth
+              title={'Save document'}
+            />
+          </ValidatedSubmitButton>
+        </div>
+      </BottomButtonPanel>
+    </div>
+  );
 };
 
 const LaborContractWriteForm = ({
@@ -45,6 +184,7 @@ const LaborContractWriteForm = ({
   userOwnerPostId,
 }: LaborContractFormProps) => {
   const currentDocumentId = useParams().id;
+  const [step, setStep] = useState(1);
 
   // useForm 훅 사용
   const methods = useForm<LaborContractEmployeeInfo>({
@@ -95,54 +235,63 @@ const LaborContractWriteForm = ({
     };
   };
 
-  // 필드 타입에 따른 입력 컴포넌트 렌더링
-  const renderFormField = (field: LaborContractFormField) => {
-    return renderField<LaborContractEmployeeInfo>({
-      field,
-      name: field.name as keyof LaborContractEmployeeInfo,
-    });
-  };
-
   // 폼이 비활성화되어야 하는지 여부
   const isFormDisabled = postPending || updatePending;
 
+  const title = useMemo(() => {
+    switch (step) {
+      case 1:
+        return `Please check my information\nfor the labor contract.`;
+
+      case 2:
+        if (document?.employer_information) {
+          return 'This is the part\ncompleted by the employer.';
+        }
+        return `The employer will now\nfills out rest.`;
+      case 3:
+        return `Complete the document\nwith your signature.`;
+      default:
+        return '';
+    }
+  }, [step]);
+
   return (
     <FormProvider {...methods}>
+      <div className="fixed top-14 left-0 w-full h-[0.625rem] flex items-center justify-center bg-surface-base z-10">
+        <ProgressStepper totalCount={3} currentStep={step} />
+      </div>
+      <PageTitle
+        title={title}
+        content={
+          step === 3 && !document?.employer_information
+            ? 'When you click the submit button,\nthe document will be sent to the employer.'
+            : ''
+        }
+      />
       <form
         className={`w-full flex flex-col px-4 ${isFormDisabled ? 'overflow-hidden pointer-events-none' : ''}`}
         onSubmit={(e) => e.preventDefault()}
       >
-        <div className="first-letter:[&>*:last-child]:mb-40 flex flex-col gap-4">
-          {/* 유학생 작성 정보 */}
-          {LaborContractFormFields.map((field) => (
-            <InputLayout key={field.name} title={field.title}>
-              {renderFormField(field)}
-            </InputLayout>
-          ))}
-          {/* 고용주 정보가 있다면 표시 */}
-          {document?.employer_information && (
-            <EmployerInfoSection
-              employ={document.employer_information}
-              type={DocumentType.LABOR_CONTRACT}
+        <div className="flex flex-col gap-4">
+          {isEdit && (
+            <InfoBanner
+              size={InfoBannerSize.MD}
+              hasIcon
+              state={InfoBannerState.POSITIVE}
+              text="Your previously entered information has been auto-filled."
             />
           )}
-        </div>
-
-        <BottomButtonPanel>
-          {/* 입력된 정보의 유효성 검사 통과 시 활성화 */}
-          <ValidatedSubmitButton
-            fieldNames={REQUIRED_FIELDS}
-            validationFn={validateLaborContract}
-            onClick={handleSubmit(handleNext)}
-          >
-            <Button
-              type={Button.Type.PRIMARY}
-              size={Button.Size.LG}
-              isFullWidth
-              title={'Complete'}
+          {step === 1 && <Step1 setStep={setStep} />}
+          {step === 2 && (
+            <Step2
+              setStep={setStep}
+              document={document as LaborContractDataResponse}
             />
-          </ValidatedSubmitButton>
-        </BottomButtonPanel>
+          )}
+          {step === 3 && (
+            <Step3 setStep={setStep} onSubmit={handleSubmit(handleNext)} />
+          )}
+        </div>
       </form>
     </FormProvider>
   );
